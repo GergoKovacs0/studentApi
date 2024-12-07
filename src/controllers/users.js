889345cfcd2e52a0eea0386f90abf49d1d59bbca
GergoKovacs0 <c9kovger@vasvari.org>
2024-12-07 17:48:40 +0100

    added sceleton for users controller

---
+++ src/controllers/users.js
@@ -0,0 +1,9 @@
+export const getAllUsers = async (req, res) => {};
+
+export const getUserById = async (req, res) => {};
+
+export const createUser = async (req, res) => {};
+
+export const updateUser = async (req, res) => {};
+
+export const deleteUser = async (req, res) => {};
